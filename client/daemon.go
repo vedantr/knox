eb21b351d0dc214edbd83aaf4b2f8d28f8ee6772
Cedric Staub <cstaub@pinterest.com>
2020-06-25 16:26:54 -0700

    Add a lot more logging to the daemon to help in debugging

--- client/daemon.go
+++ client/daemon.go
@@ -92,18 +92,21 @@ func (d *daemon) loop(refresh time.Duration) {
 	watcher.Add(d.registerFilename())
 
 	for {
-		logf("Updating keys")
+		logf("Daemon updating all registered keys")
+		start := time.Now()
 		err := d.update()
 		if err != nil {
 			d.updateErrCount++
-			logf("Failed to update: %s", err.Error())
+			logf("Failed to update keys: %s", err.Error())
 		} else {
 			d.successCount++
 		}
+		logf("Update of keys completed after %d ms", time.Since(start).Milliseconds())
 
 		select {
-		case <-watcher.Events:
-			// on any change to register file
+		case event := <-watcher.Events:
+			// On any change to register file
+			logf("Got file watcher event: %s on %s", event.Op.String(), event.Name)
 		case <-t.C:
 			// add random jitter to prevent a stampede
 			<-time.After(time.Duration(rand.Intn(10)) * time.Millisecond)
@@ -167,14 +170,25 @@ func (d *daemon) update() error {
 	if err != nil {
 		return err
 	}
+	logf("Requested keys: %s", keyIDs)
 
 	keyMap := map[string]string{}
+	existingKeys := map[string]bool{}
 	for _, k := range keyIDs {
 		// set default value to empty string
 		keyMap[k] = ""
+		existingKeys[k] = false
 	}
+
 	currentKeyIDs, err := d.currentRegisteredKeys()
+	if err != nil {
+		return err
+	}
+	logf("Current keys on disk: %s", currentKeyIDs)
+
 	for _, keyID := range currentKeyIDs {
+		existingKeys[keyID] = true
+
 		if _, present := keyMap[keyID]; present {
 			key, err := d.cli.CacheGetKey(keyID)
 			if err != nil {
@@ -187,13 +201,17 @@ func (d *daemon) update() error {
 			d.deleteKey(keyID)
 		}
 	}
+
 	if len(keyMap) > 0 {
 		updatedKeys, err := d.cli.GetKeys(keyMap)
 		if err != nil {
 			return err
 		}
+		logf("Updated keys received from server: %s", updatedKeys)
 		for _, k := range updatedKeys {
 			err = d.processKey(k)
+			existingKeys[k] = true
+
 			if err != nil {
 				// Keep going in spite of failure
 				d.getKeyErrCount++
@@ -201,6 +219,16 @@ func (d *daemon) update() error {
 			}
 		}
 	}
+	// Find out if we missed anything (useful for humans reading the logs)
+	// If key was not processed, and is also not current, then it didn't exist
+	notFound := []string{}
+	for id, exists := range existingKeys {
+		if !exists {
+			notFound = append(notFound, id)
+		}
+	}
+	logf("Keys not found on server: %s", notFound)
+
 	return nil
 }
 
